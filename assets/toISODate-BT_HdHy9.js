609201cc8811864937fb69b2f80c4732846971de
AmukhaTech2026 <kirankumar.sahu@amukha.com>
2026-08-18 10:35:15 +0530

    Delete assets directory

--- assets/toISODate-BT_HdHy9.js
+++
@@ -1,1 +0,0 @@
-function o(n){if(!n)return"";const t=new Date(n),e=t.getFullYear(),r=String(t.getMonth()+1).padStart(2,"0"),a=String(t.getDate()).padStart(2,"0");return`${e}-${r}-${a}`}export{o as t};
